88a94959d2f60d7f17f29c94f47c9aef489fbd38
Alessandro Marzoli <a.marzoli@ambrogio.com>
2018-07-11 12:09:29 +0200

    gyb

---
+++ Vendor/countries.swift.gyb.py
@@ -0,0 +1,27 @@
+% warning = "This file was automatically generated and should not be edited."
+// ${warning}
+
+let _countries = [
+%{
+  import json
+}%
+% with open('../Resources/countries.json') as file:
+ % for country in json.load(file):
+  %{
+  name = country["name"].encode('utf-8')
+  phoneCode = country.get("phoneCode")
+  iso = country["iso"]
+  countryCode = country.get('countryCode')
+  }%
+  % if phoneCode and countryCode:
+  Country(name: "${name}", iso: "${iso}", phoneCode: ${phoneCode}, countryCode: ${countryCode}),
+  % elif phoneCode is not None:
+  Country(name: "${name}", iso: "${iso}", phoneCode: ${phoneCode}, countryCode: nil),
+  % elif countryCode is not None:
+  Country(name: "${name}", iso: "${iso}", phoneCode: nil, countryCode: ${countryCode}),
+  % else:
+  Country(name: "${name}", iso: "${iso}", phoneCode: nil, countryCode: nil),
+  %end
+ %end
+%end
+]
